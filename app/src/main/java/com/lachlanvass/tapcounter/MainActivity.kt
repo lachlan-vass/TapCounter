18ca101bb29650d3535ae8e8dad11b7e8d12a7e7
Lachlan Vass <lachlan.vass@superretailgroup.com>
2022-07-12 14:49:49 +1000

    feat: invoke counter screen

--- app/src/main/java/com/lachlanvass/tapcounter/MainActivity.kt
+++ app/src/main/java/com/lachlanvass/tapcounter/MainActivity.kt
@@ -3,67 +3,31 @@ package com.lachlanvass.tapcounter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
-import com.lachlanvass.tapcounter.presentation.components.counter.Counter
-import com.lachlanvass.tapcounter.presentation.components.counternavigation.CounterNavigation
 import com.lachlanvass.tapcounter.presentation.components.counternavigation.NavigationRoute
+import com.lachlanvass.tapcounter.presentation.components.counterscreen.CounterScreen
 import com.lachlanvass.tapcounter.ui.theme.TapCounterTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val navigationRoutes = listOf(
+        NavigationRoute.OneCounter,
+        NavigationRoute.TwoCounter
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        loadCounterUi(numberOfCounters = 2)
-    }
-
-    private fun loadCounterUi(numberOfCounters: Int) {
-
+        
         setContent {
             TapCounterTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-
-                    Scaffold(
-
-                        drawerContent = {
-                            CounterNavigation(listOf(
-                                NavigationRoute.OneCounter,
-                                NavigationRoute.TwoCounter
-                            ))
-                        }
-                    ) {
-
-                    }
-
-                    Column() {
-
-                        repeat(numberOfCounters) {
-                            Counter()
-                        }
-
-                    }
-
-                }
+                
+                CounterScreen(
+                    numberOfOnScreenCounters = 2,
+                    navigationRoutes = this.navigationRoutes )
             }
         }
 
+
     }
+
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    TapCounterTheme {
-//        Greeting("Android")
-//    }
-//}
